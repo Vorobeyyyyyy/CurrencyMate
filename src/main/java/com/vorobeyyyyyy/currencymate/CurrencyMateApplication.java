05214089bd7e38032033bd6e75a67b36efcbc415
Vorobeyyyyyy <mrskilkchat@gmail.com>
2024-06-25 03:08:17 +0300

    tz

--- src/main/java/com/vorobeyyyyyy/currencymate/CurrencyMateApplication.java
+++ src/main/java/com/vorobeyyyyyy/currencymate/CurrencyMateApplication.java
@@ -1,5 +1,7 @@
 package com.vorobeyyyyyy.currencymate;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CurrencyMateApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Europe/Minsk"));
 		SpringApplication.run(CurrencyMateApplication.class, args);
 	}
 
